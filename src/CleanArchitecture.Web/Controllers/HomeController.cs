05f8cd00bbff56afed5c6846f74ec62eee10bb61
Tony Hume <hume.tony@gmail.com>
2017-03-07 15:34:24 -0500

    Lab 0-2 work

--- src/CleanArchitecture.Web/Controllers/HomeController.cs
+++ src/CleanArchitecture.Web/Controllers/HomeController.cs
@@ -1,16 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Guestbook> _guestbookRepository;
+
+        public HomeController(IRepository<Guestbook> guestbookRepository)
+        {
+            _guestbookRepository = guestbookRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            if(!_guestbookRepository.List().Any())
+            {
+                var newGuestbook = new Guestbook()
+                {
+                    Name = "My Guestbook"
+                };
+
+                newGuestbook.Entries.Add(new GuestbookEntry { DateTimeCreated = DateTime.UtcNow.AddHours(-2), EmailAddress = "test@test.com", Message = "Hello Pips!" });
+                newGuestbook.Entries.Add(new GuestbookEntry { DateTimeCreated = DateTime.UtcNow.AddHours(-1), EmailAddress = "qa@test.com", Message = "Hello Pips!!" });
+                newGuestbook.Entries.Add(new GuestbookEntry { EmailAddress = "test2@test.com", Message = "Hello Pips!!!" });
+
+                _guestbookRepository.Add(newGuestbook);
+            }
+
+            var guestbook = _guestbookRepository.GetById(1);
+
+            var viewmodel = new HomePageViewModel();
+            viewmodel.GuestbookName = guestbook.Name;
+            viewmodel.PreviousEntries.AddRange(guestbook.Entries);
+
+            return View(viewmodel);
         }
 
         public IActionResult About()
